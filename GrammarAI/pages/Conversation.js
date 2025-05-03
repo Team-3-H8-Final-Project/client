4c98ec22dd33f97a46e3da7ca2df9848e19114a4
rheinatamara <rheina.tamara@outlook.com>
2025-05-03 17:19:30 +0700

    feat: Tab navigator, Challenge page, files for other pages

---
+++ GrammarAI/pages/Conversation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const Conversation = () => {
+  return (
+    <View>
+      <Text>Conversation</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Conversation;
