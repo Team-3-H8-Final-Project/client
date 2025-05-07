e01ae9eb9eb09ecc943a7cdba31f33d07f56ab57
Asep Saefuddin <80612288+asepsaefuddin@users.noreply.github.com>
2025-05-07 22:34:51 +0700

    Update ChallengeStack.js

--- GrammarAI/navigation/ChallengeStack.js
+++ GrammarAI/navigation/ChallengeStack.js
@@ -1,13 +1,9 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Challenges from "../pages/Challenges";
 import ChallengeDetail from "../pages/ChallengeDetail";
-
 import Feedback from "../pages/Feedback";
 import FeedbackGrammar from "../pages/FeedbackGrammar";
 import Grammar from "../pages/Grammar";
-
-import ChallengeFeedback from "../pages/ChallengeFeedback";
-
 
 const Stack = createNativeStackNavigator();
 
@@ -16,13 +12,9 @@ export default function ChallengesStack() {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="ChallengeMain" component={Challenges} />
       <Stack.Screen name="ChallengeDetail" component={ChallengeDetail} />
-
       <Stack.Screen name="Feedback" component={Feedback} />
       <Stack.Screen name="Grammar" component={Grammar} />
       <Stack.Screen name="FeedbackGrammar" component={FeedbackGrammar} />
-
-      <Stack.Screen name="ChallengeFeedback" component={ChallengeFeedback} />
-
     </Stack.Navigator>
   );
 }
